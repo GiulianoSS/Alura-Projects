4c7c2619e9493d5ed2b7d35ebcb3d994f179ee8e
GiulianoSS <giuliano.ss@hotmail.com>
2025-02-03 17:32:17 -0300

    refactor: create 'exibir_subtitulo' function to improve screen clearing and messaging flexibility

--- Primeira_Aplicacao/app.py
+++ Primeira_Aplicacao/app.py
@@ -1,6 +1,6 @@
 import os
 
-restaurantes = ['Pizza', '']
+restaurantes = ['Pizza', ]
 
 def exibir_nome_do_programa():
     print("""
@@ -14,29 +14,34 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls')
-    print('Finalizando o app\n')
+    exibir_subtitulo('Finalizando app')
+
+def voltar_menu():
+    input('\nDigite uma tecla para voltar ao menu principal: ')
+    main()
 
 def opcao_invalida():
     print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal: ')
+    voltar_menu()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
 
 def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('Cadastro de novos restaurantes\n')
+    exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('Digite uma tecla para voltar ao menu principal: ')
-    main()
+    voltar_menu()
 
-def listar_restaurantes()
-    os.system('cls')
-    print('Listando restaurantes\n')
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes')
+
     for restaurante in restaurantes:
         print(f'{restaurante}')
-    input('Digite uma tecla para voltar ao menu principal: ')
-    main()
+    voltar_menu()
 
 def escolher_opcoes():
     try:
