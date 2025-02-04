6ee27f399fcd828de3d2dd3f3078ff8e45458038
GiulianoSS <giuliano.ss@hotmail.com>
2025-02-04 17:06:18 -0300

    feat: change 'restaurantes' from list to dict and display category and status

--- Primeira_Aplicacao/app.py
+++ Primeira_Aplicacao/app.py
@@ -1,6 +1,8 @@
 import os
 
-restaurantes = ['Pizza', ]
+restaurantes = [{'nome':'Praça', 'categoria':'Japonesa','ativo':False},
+                {'nome':'Pizza Suprema', 'categoria':'Pizza','ativo':True},
+                {'nome':'Cantina', 'categoria':'Italiano','ativo':False}]
 
 def exibir_nome_do_programa():
     print("""
@@ -40,7 +42,10 @@ def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
 
     for restaurante in restaurantes:
-        print(f'{restaurante}')
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = restaurante['ativo']
+        print(f'- {nome_restaurante} | {categoria} | {ativo}')
     voltar_menu()
 
 def escolher_opcoes():
