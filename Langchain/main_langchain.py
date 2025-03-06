aa7f15ef107c6ea1b4658d7c39c7b78eb20536d0
GiulianoSS <giuliano.ss@hotmail.com>
2025-03-06 15:56:28 -0300

    feat: add PromptTemplate for dynamic prompt generation

--- Langchain/main_langchain.py
+++ Langchain/main_langchain.py
@@ -1,11 +1,20 @@
 from langchain_ollama import ChatOllama
+from langchain.prompts import PromptTemplate
 
 # Variables
 numero_de_dias = 7
 numero_de_criancas = 2
 atividade = "praia"
 
-prompt = f"Crie um roteiro de viagem de {numero_de_dias} dias, para uma família com {numero_de_criancas} crianças, que gostam de {atividade}."
+template = PromptTemplate.from_template(
+    f"Crie um roteiro de viagem de {numero_de_dias} dias, para uma família com {numero_de_criancas} crianças, que gostam de {atividade}."
+)
+
+prompt = template.format(dias = numero_de_dias,
+                        criancas = numero_de_criancas,
+                        atividade = atividade)
+
+# prompt = f"Crie um roteiro de viagem de {numero_de_dias} dias, para uma família com {numero_de_criancas} crianças, que gostam de {atividade}."
 print(prompt)
 
 llm = ChatOllama(
